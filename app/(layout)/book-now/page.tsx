00bccf86d87c4c9c32aa8101380bb4c1e571bbdd
Asma Siddique <asmasiddique44097@gmail.com>
2025-09-07 23:13:01 +0500

    update ui changessss

--- app/(layout)/book-now/page.tsx
+++ app/(layout)/book-now/page.tsx
@@ -1,15 +1,18 @@
 import FeatureSection from "@/component/sections/FeatureSection";
 import HeroSection from "@/component/sections/HeroSection";
+import { Suspense } from "react";
 
 export default function BookNow() {
   return (
     <>
-      <HeroSection
-        height="75vh"
-        bgImage="/ccc.PNG"
-        title="Book Your Transfer Now!"
-        description="Secure your transfer to La Plagne Resorts today with our fast, reliable, and comfortable service."
-      />
+      <Suspense fallback={<div>Loading booking form...</div>}>
+        <HeroSection
+          height="75vh"
+          bgImage="/ccc.PNG"
+          title="Book Your Transfer Now!"
+          description="Secure your transfer to La Plagne Resorts today with our fast, reliable, and comfortable service."
+        />
+      </Suspense>
       <FeatureSection />
     </>
   )
